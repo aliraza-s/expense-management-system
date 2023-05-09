6bd1723840bfc8a4bd844924ec22bb38d98ce9dd
Ali Raza <alirazathor@gmail.com>
2023-05-09 11:22:27 +0500

    intial commit

---
+++ inc/container.php
@@ -0,0 +1,26 @@
+<link rel="icon" type="image/png" href="https://gemprogrammers.com/wp-content/uploads/2023/01/2.png">
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <p><a href="https://gemprogrammers.com/"><img src="https://gemprogrammers.com/wp-content/uploads/2023/01/2.png" style="width:100px;height:50px;"></a></p>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Home</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	
