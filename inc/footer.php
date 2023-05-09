13d06c0e13acf11bb0d0b4bfa6a3aacfe0725290
Ali Raza <alirazathor@gmail.com>
2023-05-09 11:59:48 +0500

    third commit

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <div class="insert-post-ads1" style="margin-top:20px;">
 <div class="ali" style="text-align:center; margin-bottom:10px;">
-    	&#169; Copyright 2023, All Right Reserved. Develop by <a href="https://gemprogrammers.com/">Gem Programmers</a>
+    	&#169; Copyright, All Right Reserved. Develop by <a href="https://gemprogrammers.com/">Gem Programmers</a>
 </body>
 </html>
 
